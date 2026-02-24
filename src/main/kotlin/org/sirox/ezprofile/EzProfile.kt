b03e6b7d965adb31ac1b1f08007cdb5010cb285f
403 <neststorm136@gmail.com>
2026-02-24 18:48:40 +0100

    Initial commit

---
+++ src/main/kotlin/org/sirox/ezprofile/EzProfile.kt
@@ -0,0 +1,14 @@
+package org.sirox.ezprofile
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class EzProfile : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
